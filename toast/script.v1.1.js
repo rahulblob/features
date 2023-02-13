4de1a79b2dc84b953fe763d5776f800529c1fb28
Rahul Lodhi <123192165+rahulblob@users.noreply.github.com>
2023-02-13 22:11:53 +0530

    Update and rename script.v1.js to script.v1.1.js

---
+++ toast/script.v1.1.js
@@ -0,0 +1,2 @@
+function toast(content){document.querySelector('.toast').innerHTML = `<span>${content}</span>`;}
+document.querySelector(".button").style.background = document.querySelector("button").getAttribute("data-background");
