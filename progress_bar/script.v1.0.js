b903087497f90e961abf401b88460eceeffb9ce3
Rahul Lodhi <123192165+rahulblob@users.noreply.github.com>
2023-02-14 15:49:45 +0530

    Create script.v1.0.js

---
+++ progress_bar/script.v1.0.js
@@ -0,0 +1,11 @@
+let divC = document.createElement("div");
+divC.classList = "progress";
+divC.id = "PreLoaderBar";
+  divC.innerHTML = "<div class='indeterminate'/>";
+  document.body.appendChild(divC);
+document.onreadystatechange = function () {
+            if (document.readyState === "complete") {
+                console.log(document.readyState);
+                document.getElementById("PreLoaderBar").style.display = "none";
+            }
+        }
