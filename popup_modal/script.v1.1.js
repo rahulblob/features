48efcae9b833360f3579151164713147b4944048
Rahul Lodhi <123192165+rahulblob@users.noreply.github.com>
2023-02-14 23:10:15 +0530

    Delete popup_modal directory

--- popup_modal/script.v1.1.js
+++
@@ -1,6 +0,0 @@
-let popCreate = document.createElement("div");
-popCreate.classList = "popupC";
-popCreate.innerHTML = `<div class="span"><div class="popC">&#10005</div><span id="popT"></span></div>`;
-document.body.appendChild(popCreate);
-function popupToggle(message){document.querySelector("#popT").innerHTML = message;document.querySelector(".popupC").style.opacity = "1";document.querySelector(".popupC").style.visibility = "visible";}
-document.querySelector(".popC").addEventListener("click",function(){document.querySelector(".popupC").style.opacity = "0";document.querySelector(".popupC").style.visibility = 'hidden'})
