485a9d053b09c3714c156f02d429eac1f8a1366f
Rahul Lodhi <123192165+rahulblob@users.noreply.github.com>
2023-02-14 16:19:30 +0530

    Update and rename script.v1.7.js to script.v1.8.js

--- progress_bar/script.v1.8.js
+++ progress_bar/script.v1.8.js
@@ -7,6 +7,6 @@ document.onreadystatechange = function () {
             if (document.readyState === "complete") {
                 console.log(document.readyState);
                 document.getElementById("PreLoaderBar").id = "";
-                divC.classList = "";
+                divC = "";
           }
         }
