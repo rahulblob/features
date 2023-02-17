951bd7a9969f7737147ff16a0cb7093910675f77
Rahul Lodhi <123192165+rahulblob@users.noreply.github.com>
2023-02-18 00:05:53 +0530

    Update and rename script.v3.4.js to script.v3.5.js

--- resources/script.v3.5.js
+++ resources/script.v3.5.js
@@ -6,8 +6,7 @@ function popupToggle(message){document.querySelector("#popT").innerHTML = messag
 document.querySelector(".popC").addEventListener("click",function(){document.querySelector(".popupC").style.opacity = "0";document.querySelector(".popupC").style.visibility = 'hidden'});
 function progressHide(){document.querySelector(".progress").style.display = "none";};
 function toast(content){let toastD = document.createElement("div");toastD.classList = "toast";document.body.appendChild(toastD);document.querySelector('.toast').innerHTML = `<span>${content}</span>`;};
-function rotaSettings(colour,hideIn){document.querySelector(".rota").style.borderColor = colour;document.querySelector(".rota").style.borderLeft = "3px solid rgba(255,255,255,.0)";setTimeout(()=>{document.querySelector(".rota").style.opacity = "0";document.querySelector(".rota").style.visibility = "hidden";},hideIn)};
-document.onreadystatechange = function () {
+function rotaSetting(color){$(".rota").css("border",`3px solid ${color}`).css("border-bottom","3px solid rgba(255,255,255,.0)")};document.onreadystatechange = function () {
 if(document.readyState === "complete") {
 console.log(document.readyState);
 document.getElementById("PreLoaderBar").style.display = "none";
