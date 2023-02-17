7cf17c57fec938204b5b0c4548c51837bdcf0f1d
Rahul Lodhi <123192165+rahulblob@users.noreply.github.com>
2023-02-18 00:34:12 +0530

    Update and rename script.js to script.v1.js

--- js/script.v1.js
+++ js/script.v1.js
@@ -6,7 +6,4 @@ function popupToggle(message){document.querySelector("#popT").innerHTML = messag
 document.querySelector(".popC").addEventListener("click",function(){document.querySelector(".popupC").style.opacity = "0";document.querySelector(".popupC").style.visibility = 'hidden'});
 function progressHide(){document.querySelector(".progress").style.display = "none";};
 function toast(content){let toastD = document.createElement("div");toastD.classList = "toast";document.body.appendChild(toastD);document.querySelector('.toast').innerHTML = `<span>${content}</span>`;};
-if(document.readyState === "complete") {
-console.log(document.readyState);
-document.getElementById("PreLoaderBar").style.display = "none";
-}};
+if(document.readyState === "complete") {document.getElementById("PreLoaderBar").style.display = "none";};
