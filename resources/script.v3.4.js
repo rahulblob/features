747b34b43b3f755322a2d875d033705500c1b2ba
Rahul Lodhi <123192165+rahulblob@users.noreply.github.com>
2023-02-17 21:38:01 +0530

    Update and rename script.v3.3.js to script.v3.4.js

--- resources/script.v3.4.js
+++ resources/script.v3.4.js
@@ -5,10 +5,10 @@ document.body.appendChild(popCreate);
 function popupToggle(message){document.querySelector("#popT").innerHTML = message;document.querySelector(".popupC").style.opacity = "1";document.querySelector(".popupC").style.visibility = "visible";}
 document.querySelector(".popC").addEventListener("click",function(){document.querySelector(".popupC").style.opacity = "0";document.querySelector(".popupC").style.visibility = 'hidden'});
 function progressHide(){document.querySelector(".progress").style.display = "none";};
-function toast(content){let toastD = document.createElement("div");toastD.classList = "toast";document.body.appendChild(toastD);document.querySelector('.toast').innerHTML = `<span>${content}</span>`;}
-function rotaSettings(colour,hideIn){document.querySelector(".rota").style.borderColor = colour;document.querySelector(".rota").style.borderLeft = "3px solid rgba(255,255,255,.0)";setTimeout(()=>{document.querySelector(".rota").style.opacity = "0";document.querySelector(".rota").style.visibility = "hidden";},hideIn || none)}
+function toast(content){let toastD = document.createElement("div");toastD.classList = "toast";document.body.appendChild(toastD);document.querySelector('.toast').innerHTML = `<span>${content}</span>`;};
+function rotaSettings(colour,hideIn){document.querySelector(".rota").style.borderColor = colour;document.querySelector(".rota").style.borderLeft = "3px solid rgba(255,255,255,.0)";setTimeout(()=>{document.querySelector(".rota").style.opacity = "0";document.querySelector(".rota").style.visibility = "hidden";},hideIn)};
 document.onreadystatechange = function () {
 if(document.readyState === "complete") {
 console.log(document.readyState);
 document.getElementById("PreLoaderBar").style.display = "none";
-}}
+}};
