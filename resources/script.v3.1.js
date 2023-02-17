d3fe9f460bc6d60cc7cb9efcf8434302adcb54b5
Rahul Lodhi <123192165+rahulblob@users.noreply.github.com>
2023-02-17 21:33:12 +0530

    Update script.v3.1.js

--- resources/script.v3.1.js
+++ resources/script.v3.1.js
@@ -6,7 +6,7 @@ function popupToggle(message){document.querySelector("#popT").innerHTML = messag
 document.querySelector(".popC").addEventListener("click",function(){document.querySelector(".popupC").style.opacity = "0";document.querySelector(".popupC").style.visibility = 'hidden'});
 function progressHide(){document.querySelector(".progress").style.display = "none";};
 function toast(content){let toastD = document.createElement("div");toastD.classList = "toast";document.body.appendChild(toastD);document.querySelector('.toast').innerHTML = `<span>${content}</span>`;}
-function rotaSettings(colour,hideIn){document.querySelector(".rota").style.borderColor = colour;document.querySelector(".rota").style.borderLeft = "3px solid rgba(255,255,255,.0)";setTimeout(()=>{document.querySelector(".rota").style.opacity = "0";document.querySelector(".rota").style.visibility = "hidden";},hideIn)}
+function rotaSettings(colour,hideIn){document.querySelector(".rota").style.borderColor = colour;document.querySelector(".rota").style.borderLeft = "3px solid rgba(255,255,255,.0)";setTimeout(()=>{document.querySelector(".rota").style.opacity = "0";document.querySelector(".rota").style.visibility = "hidden";},hideIn || none)}
 document.onreadystatechange = function () {
 if(document.readyState === "complete") {
 console.log(document.readyState);
