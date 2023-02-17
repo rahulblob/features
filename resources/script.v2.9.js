910cdf12ac4c544f83eb13a23603778f7ff6a0da
Rahul Lodhi <123192165+rahulblob@users.noreply.github.com>
2023-02-18 00:24:03 +0530

    Update and rename script.v1.9.js to script.v2.9.js

--- resources/script.v2.9.js
+++ resources/script.v2.9.js
@@ -2,7 +2,7 @@ let popCreate = document.createElement("div");
 popCreate.classList = "popupC";
 popCreate.innerHTML = `<div class="span"><div class="popC">&#10005</div><span id="popT"></span></div>`;
 document.body.appendChild(popCreate);
-function popupToggle(message){document.querySelector("#popT").innerHTML = message;document.querySelector(".popupC").style.opacity = "1";document.querySelector(".popupC").style.visibility = "visible";}
+function popupToggle(message){document.querySelector("#popT").innerHTML = message;document.querySelector(".popupC").style.opacity = "1";document.querySelector(".popupC").style.visibility = "visible";};
 document.querySelector(".popC").addEventListener("click",function(){document.querySelector(".popupC").style.opacity = "0";document.querySelector(".popupC").style.visibility = 'hidden'});
 function progressHide(){document.querySelector(".progress").style.display = "none";};
 function toast(content){let toastD = document.createElement("div");toastD.classList = "toast";document.body.appendChild(toastD);document.querySelector('.toast').innerHTML = `<span>${content}</span>`;};
